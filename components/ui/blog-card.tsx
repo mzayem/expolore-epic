a5bce515c959c44f7118e7c81a2cc68a7ec56569
mzayem <mzayemazam@gmail.com>
2024-06-05 22:10:16 +0500

    fixes in card

--- components/ui/blog-card.tsx
+++ components/ui/blog-card.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { Blog, Image } from "@prisma/client";
-
-import { Card, CardContent, CardDescription, CardTitle } from "./card";
 import ImageComponent from "next/image";
 import { Button } from "./button";
 
@@ -28,42 +26,37 @@ export default function BlogCard({ data }: BlogListProps) {
   if (!isMounted) {
     return null;
   }
+
   return (
-    <>
-      <div className="flex-col">
-        {data.map((blog) => (
-          <Card
+    <div className="grid grid-cols-1 p-5 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {data.map((blog) => (
+        <div key={blog.id} className="overflow-hidden rounded-lg shadow-md">
+          <div
             onClick={() => {
               router.push(`/blogs/${blog.id}`);
             }}
-            key={blog.id}
-            className=" flex flex-col space-y-5 my-10 w-full "
+            className="cursor-pointer"
           >
-            <CardContent className="flex flex-row gap-10 py-5">
-              <ImageComponent
-                src={blog?.images?.[0]?.url}
-                alt="image"
-                width={70}
-                height={70}
-                className="aspect-square justify-center object-cover w-28 h-28 rounded-md"
-              />
-              <div className="flex flex-col justify-center w-3/5 space-y-3 ">
-                <div>
-                  <CardTitle>{blog.title}</CardTitle>
-                  <p className=" text-gray-500 py-2">Author: {blog?.name}</p>
-                </div>
-
-                <CardDescription className="text-gray-700">
-                  {blog?.content.substring(0, 50)}
-                </CardDescription>
+            <ImageComponent
+              src={blog?.images?.[0]?.url}
+              alt="image"
+              width={500}
+              height={300}
+              className="object-cover w-full h-64"
+            />
+            <div className="p-4 flex flex-col items-center justify-center">
+              <h3 className="text-xl font-semibold mb-2">{blog.title}</h3>
+              <p className="text-gray-500 mb-2">Author: {blog?.name}</p>
+              <p className="text-gray-700">{blog?.content.substring(0, 100)}</p>
+              <div className="py-4 items-end justify-end">
+                <Button onClick={() => router.push(`/blogs/${blog.id}`)}>
+                  View Post
+                </Button>
               </div>
-              <div className=" flex flex-col w-1/5 justify-center">
-                <Button>view post</Button>
-              </div>
-            </CardContent>
-          </Card>
-        ))}
-      </div>
-    </>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
