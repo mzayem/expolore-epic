646fbfc7ea0cef1a999673c30d904c5061702b6f
mzayem <mzayemazam@gmail.com>
2024-06-05 22:16:50 +0500

    web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import NextTopLoader from "nextjs-toploader";
+import { Analytics } from "@vercel/analytics/react";
 
 const font = Urbanist({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <NextTopLoader />
+        <Analytics />
         <Navbar />
         {children}
         <Footer />
